d49cce013e21b75a10dd974a57c6ce39b97bfd35
highjack <highjack@users.noreply.github.com>
2022-08-26 09:01:13 +0100

    wrapping add_wordlist in a function

--- run.sh
+++ run.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+function add_wordlist(){
+if [ "$3" == "git" ]; then
+	git clone $1 $2 2>/dev/null
+else
+	wget $1 -O $2 2>/dev/null
+fi
+cd $2 2>/dev/null
+git pull 2>/dev/null
+cd ../..
+}
+
 echo "[+] getting latest version of technodrome"
 git pull
 echo "[+] downloading wordlists"
-git clone https://github.com/danielmiessler/SecLists.git ./wordlists/SecLists 2>/dev/null
-cd ./wordlists/SecLists 2>/dev/null
-git pull 2>/dev/null
-cd ../..
+add_wordlist("https://github.com/danielmiessler/SecLists.git","./wordlists/SecLists","git")
+add_wordlist("https://github.com/milo2012/pathbrute.git","./wordlists/pathbrute","git")
 echo "[+] updating environmental variables from configs/env"
 file="./configs/env"
 zsh="./configs/zsh"
